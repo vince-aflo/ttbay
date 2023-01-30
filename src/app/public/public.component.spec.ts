8206e9cad76a1d4ca7a9462bb7dda4ff9611d33d
Ernest Lamptey <ernest.lamptey@turntabl.io>
2023-01-30 17:02:14 +0000

    add karma.conf.js file

--- src/app/public/public.component.spec.ts
+++ src/app/public/public.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PublicComponent } from './public.component';
 
@@ -8,6 +9,9 @@ describe('PublicComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ PublicComponent ]
     })
     .compileComponents();
