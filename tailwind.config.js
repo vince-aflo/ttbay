8d0f91f0c92d7f364064c869c2b9e251bdd9d8e2
Ernest Lamptey <ernest.lamptey@turntabl.io>
2023-01-30 15:06:50 +0000

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif']
+    },
+    colors: {
+      'green': '#bdff00',
+      'orange': '#ffd6bb',
+      'violet': '#b0b0ff',
+      'blue': '#b8ffee',
+      'grey': '#6d6e71'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
