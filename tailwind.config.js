c2925209851ccf130090dab994e7ffaa7827f40e
Ernest Lamptey <ernest.lamptey@turntabl.io>
2023-01-31 11:51:15 +0000

    add mobile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,14 @@ module.exports = {
       sans: ['Work Sans', 'sans-serif']
     },
     colors: {
+      'black': '#000',
       'green': '#bdff00',
       'orange': '#ffd6bb',
       'violet': '#b0b0ff',
       'blue': '#b8ffee',
       'grey': '#6d6e71',
       'lightgrey': '#ededed',
+      'white': '#fff'
     },
     extend: {},
   },
