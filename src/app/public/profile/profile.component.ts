9e1f87e415c8e3f69a6ed7ea17ed3b2969364cc1
Ernest Lamptey <ernest.lamptey@turntabl.io>
2023-02-06 09:18:13 +0000

    add preview profile picture

--- src/app/public/profile/profile.component.ts
+++ src/app/public/profile/profile.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
-
+  showPreview(event:any) {
+    if(event.target.files.length > 0) {
+      let src = URL.createObjectURL(event.target.files[0]);
+      let preview:any = document.getElementById("img-preview");
+      preview.src = src;
+      console.log(event.target.files);
+      console.log(preview);
+    }
+    // var src = URL.createObjectURL(event.target);
+  }
 }
