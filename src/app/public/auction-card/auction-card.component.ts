e403b42c5a64ad4bcfd241eb973c539ef43a39ff
ernest-tt <122813064+ernest-tt@users.noreply.github.com>
2023-04-11 09:08:48 +0000

    fix auction detail page (#47)

--- src/app/public/auction-card/auction-card.component.ts
+++ src/app/public/auction-card/auction-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { Auction } from 'src/app/core/models/auction.model';
 
 @Component({
@@ -7,13 +8,26 @@ import { Auction } from 'src/app/core/models/auction.model';
   templateUrl: './auction-card.component.html',
   styleUrls: ['./auction-card.component.scss']
 })
-export class AuctionCardComponent {
+export class AuctionCardComponent implements OnInit {
   @Input() auction!:Auction;
+  userEmail!:string;
 
-  constructor(private router:Router) {}
+  constructor(private router:Router, private oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit(){
+    this.oidcSecurityService.getUserData().subscribe({
+      next: (data) => {
+        this.userEmail = data.email
+        console.log(this.userEmail)
+
+      }
+    })
+  }
 
   showAuctionDetails(){
-    if (this.auction.status != 'END') {
+    if (this.auction.auctioneerEmail == this.userEmail){
+      this.router.navigateByUrl(`/auction-detail/${this.auction.auctionId}`)
+    }else if (this.auction.status != 'END') {
       this.router.navigateByUrl(`/live-auction-detail/${this.auction.auctionId}`)
     } else {
       this.router.navigateByUrl(`/completed-auction-detail/${this.auction.auctionId}`)
